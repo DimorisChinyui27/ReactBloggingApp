4dcf37fa58ff3c2f3022af66b1eeba6509f559e2
DimorisChinyui27 <50267809+DimorisChinyui27@users.noreply.github.com>
2023-01-11 20:11:55 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+//To create a new json server instance use this command on the terminal "npx json-server --watch data/db.json --port 8000"
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
